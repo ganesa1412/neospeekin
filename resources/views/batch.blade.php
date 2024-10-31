a43a067c47ae65fe8efdd93b7342117ccb04fdce
Esa Dandy Afriansya <esa.dandy08@gmail.com>
2024-10-31 09:02:00 +0700

    .

--- resources/views/batch.blade.php
+++ resources/views/batch.blade.php
@@ -7,7 +7,7 @@
     		<a href="/batch/create" class="btn btn-primary">
 				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
     			Tambah
-    		{{-- </a> --}}
+    		</a>
     	</div>
         <div class="table-responsive mb-4 mt-4">
             <table id="zero-config" class="table table-hover table-striped" style="width:100%">
